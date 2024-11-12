f482e565ca07e4c36d9218ef0e4c60d8efe76bf3
FMJdev <info@fmj.dev>
2024-11-12 23:11:41 +0100

    chore: reformat scripts and docs
    This reformats the scripts in line with the '.editorconfig' removal. We're foregoing the version bump for the 'keycloak-operator' chart since this only modifies a script used to retrieve CRDs which we ship with the chart, not the CRDs itself - therefore preserving immutability. Regardless I want to avoid unnecessary version bumps.

--- scripts/lib/stdout.sh
+++ scripts/lib/stdout.sh
@@ -16,29 +16,29 @@
 
 # Write un-colored output to stdout
 log() {
-  if [[ $# -eq 1 ]]; then
-    echo -e "$1" &>/dev/null
-  else
-    echo -e "\033[1;${1}${2}\033[0m"
-  fi
+	if [[ $# -eq 1 ]]; then
+		echo -e "$1" &>/dev/null
+	else
+		echo -e "\033[1;${1}${2}\033[0m"
+	fi
 }
 
 # Write red output to stdout
 log::red() {
-  log "31m" "${1}"
+	log "31m" "${1}"
 }
 
 # Write yellow output to stdout
 log::yellow() {
-  log "33m" "${1}"
+	log "33m" "${1}"
 }
 
 # Write green output to stdout
 log::green() {
-  log "32m" "${1}"
+	log "32m" "${1}"
 }
 
 # Write cyan output to stdout
 log::cyan() {
-  log "36m" "${1}"
+	log "36m" "${1}"
 }
